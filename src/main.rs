9bd44cd423e67083b29930d86d0027525bd0c119
chetterich <hetterich.charles@gmail.com>
2025-03-15 14:26:38 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+pub struct State {
+    pub alpha: i32,  // [α] alpha in paper
+    pub beta: i32,   // [β] beta in paper
+    pub gamma: i32,  // [γ] gamma in paper
+    pub delta: i32,  // [δ] delta in paper
+    pub eta: i32,    // [η] eta in paper
+    pub iota: i32,   // [ι] iota in paper
+    pub kappa: i32,  // [κ] kappa in paper
+    pub lambda: i32, // [λ] lambda in paper
+    pub rho: i32,    // [ρ] rho in paper
+    pub tau: i32,    // [τ] tau in paper
+    pub phi: i32,    // [φ] phi in paper
+    pub chi: i32,    // [χ] chi in paper
+    pub psi: i32,    // [ψ] psi in paper
+    pub pi: i32,     // [π] pi in paper
+    pub theta: i32,  // [θ] theta in paper
+    pub xi: i32,     // [ξ] xi in paper
+}
+
+fn main() {
+    println!("Hello, world!");
+}
